152111ae2e132796ca8bb9a07f48b62f27105ac0
felixbor <felixbor@gmail.com>
2023-02-28 21:44:53 -0400

    d elete login handlebar

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -48,7 +48,7 @@ router.get('/posts/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
     console.log(post);
-    res.render('Post', {
+    res.render('post', {
       ...post,
       logged_in: req.session.logged_in
     });
